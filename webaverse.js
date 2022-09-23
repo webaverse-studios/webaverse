7233c7c46f921fb4861e59415cfb59dd5f05f4ba
gonnavis <gonnavis@163.com>
2022-09-24 03:12:01 +0800

    Clean: stats.

--- webaverse.js
+++ webaverse.js
@@ -50,10 +50,6 @@ import universe from './universe.js';
 import npcManager from './npc-manager.js';
 import settingsManager from './settings-manager.js';
 
-import Stats from 'stats.js';
-var stats = new Stats();
-document.body.appendChild(stats.dom);
-
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
 const localQuaternion = new THREE.Quaternion();
@@ -300,7 +296,6 @@ export default class Webaverse extends EventTarget {
     
     let lastTimestamp = performance.now();
     const animate = (timestamp, frame) => {
-      stats.begin();
       performanceTracker.startFrame();
 
       const _frame = () => {
@@ -373,7 +368,6 @@ export default class Webaverse extends EventTarget {
       _frame();
 
       performanceTracker.endFrame();
-      stats.end();
     }
     renderer.setAnimationLoop(animate);
 
