e52a3e3cde8a61d8716bca9709b38104406c3306
Avaer Kazmer <a@modules.io>
2022-10-09 18:27:39 -0400

    Vite build do not include metaversefile transform

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,32 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import metaversefilePlugin from 'metaversefile/plugins/rollup.js'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    metaversefilePlugin(),
-    reactRefresh(),
-  ],
-  optimizeDeps:{
-    entries: [
-      'src/*.js',
-      'src/*.jsx',
-      'avatars/*.js',
-      'avatars/vrarmik/*.js',
-      'src/components/*.js', 
-      'src/components/*.jsx', 
-      'src/tabs/*.jsx',
-      '*.js'
-    ],
-  },
-  server: {
-    fs: {
-      strict: true,
+export default defineConfig(({command, mode, ssrBuild}) => {
+  // console.log('define config', {
+  //   command,
+  //   mode,
+  //   ssrBuild,
+  // });
+  return {
+    plugins: (command === 'build' ? [] : [metaversefilePlugin()]).concat([
+      reactRefresh(),
+    ]),
+    optimizeDeps:{
+      entries: [
+        'src/*.js',
+        'src/*.jsx',
+        'avatars/*.js',
+        'avatars/vrarmik/*.js',
+        'src/components/*.js',
+        'src/components/*.jsx',
+        'src/tabs/*.jsx',
+        '*.js'
+      ],
     },
-  },
+    server: {
+      fs: {
+        strict: true,
+      },
+    },
+  };
 })
