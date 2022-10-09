8b868e2a51f11b6d8d027f4bbc194179b49fb6d2
gonnavis <gonnavis@163.com>
2022-10-10 00:37:18 +0800

    dev

--- constants.js
+++ constants.js
@@ -81,7 +81,7 @@ switch ( origin ) {
 }
 
 // export const inappPreviewHost = _inappPreviewHost;
-export const inappPreviewHost = 'https://local.webaverse.com:4000';
+export const inappPreviewHost = 'https://local.webaverse.com:3000';
 
 //
 
