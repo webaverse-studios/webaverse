43c5dafde2865ff3328e16e59711261404317c86
Avaer Kazmer <a@modules.io>
2022-10-13 16:44:26 -0400

    Add endpoints.js

---
+++ endpoints.js
@@ -0,0 +1,37 @@
+
+import {isProd} from './constants.js';
+
+export const compilerBaseUrl = // isProd ?
+  `https://compiler.webaverse.com/`
+// :
+//   `https://local-compiler.webaverse.com/`;
+
+// scenes
+export const scenesBaseUrl = isProd ?
+  `https://scenes.webaverse.com/`
+:
+  `/packages/scenes/`;
+export const defaultSceneName = 'street.scn';
+
+// characters
+export const charactersBaseUrl = isProd ?
+  `https://characters.webaverse.com/`
+:
+  `/packages/characters/`;
+export const defaultCharacterName = 'scillia.npc';
+
+// voice packs
+export const voicePacksUrl = `https://webaverse.github.io/voicepacks/all_packs.json`;
+
+// voice endpoints
+export const voiceEndpointBaseUrl = `https://voice-cw.webaverse.com/tts`;
+export const voiceEndpointsUrl = `https://raw.githubusercontent.com/webaverse/tiktalknet/main/model_lists/all_models.json`;
+
+// image generation
+export const imageAIEndpointUrl = `https://stable-diffusion.webaverse.com`;
+
+// image captioning
+export const imageCaptionAIEndpointUrl = `https://clip.webaverse.com`;
+
+// sfx generation
+export const audioAIEndpointUrl = `https://diffsound.webaverse.com`;
