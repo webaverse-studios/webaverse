af6549b7c4bd13a28ed3a70641f6f013200204eb
aryasalehi <ariasalehidev@gmail.com>
2022-10-13 12:38:17 +0330

    Cleanup texture per row calculation

--- atlasing.js
+++ atlasing.js
@@ -316,7 +316,9 @@ export const createTextureAtlas = (meshes, {
 };
 
 export const calculateCanvasAtlasTexturePerRow = (numTextures) => {
-  return Math.pow(2, Math.ceil(Math.log(numTextures) / Math.log(2)));
+  const minNumTexturesPerRow = Math.sqrt(numTextures);
+  // ? rounding the minNumTexturesPerRow to the next power of 2
+  return Math.pow(2, Math.ceil(Math.log(minNumTexturesPerRow) / Math.log(2)));
 };
 
 const _adjustAtlasTextureSettings = (
