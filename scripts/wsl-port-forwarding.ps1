895330ec2a5a2bf511a70b42e1d5d1f6ab6371dc
Avaer Kazmer <a@modules.io>
2022-10-17 18:30:23 -0400

    Add port forwarding helper script

---
+++ scripts/wsl-port-forwarding.ps1
@@ -0,0 +1,29 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {   
+  $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+$remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
+$found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+
+if ($found) {
+  $remoteport = $matches[0];
+}
+else {
+  Write-Output "IP address could not be found";
+  exit;
+}
+
+$ports = @(80, 8080, 443, 3000, 3333, 4444, 5555, 6666, 7777, 8888);
+
+for ($i = 0; $i -lt $ports.length; $i++) {
+  $port = $ports[$i];
+  Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port";
+  Invoke-Expression "netsh advfirewall firewall delete rule name=$port";
+
+  Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port connectport=$port connectaddress=$remoteport";
+  Invoke-Expression "netsh advfirewall firewall add rule name=$port dir=in action=allow protocol=TCP localport=$port";
+}
+
+Invoke-Expression "netsh interface portproxy show v4tov4";
