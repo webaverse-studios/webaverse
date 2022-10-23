bfb8127bb77424049d3bd2d99186b86c0aa223d8
aryasalehi <ariasalehidev@gmail.com>
2022-10-23 16:16:21 +0330

    Revert cleanup

--- scripts/npm-preinstall.sh
+++ scripts/npm-preinstall.sh
@@ -1,15 +1,5 @@
 echo 'Updating Packages...'
 sudo apt-get update
 echo 'Installing Packages...'
-sudo apt-get install \ 
-libatk1.0-0 \ 
-libatk-bridge2.0-0 \ 
-libxcomposite-dev \
-libxdamage1 \
-libxrandr2 \
-libgbm-dev \
-libxkbcommon-x11-0 \
-libpangocairo-1.0-0 \ 
-libasound2 \ 
-libwayland-client0 \
--y # -y flag will say YES to all prompts
+# -y flag will say YES to all prompts
+sudo apt-get install libatk1.0-0 libatk-bridge2.0-0 libxcomposite-dev libxdamage1 libxrandr2 libgbm-dev libxkbcommon-x11-0 libpangocairo-1.0-0 libasound2 libwayland-client0 -y
