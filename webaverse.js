c05c20a6fe6c618813361a633fec820ce7c87e9f
shawticus <shawmakesmagic@gmail.com>
2022-11-08 23:48:34 -0800

    Refactor game.js and call from weba init

--- webaverse.js
+++ webaverse.js
@@ -48,6 +48,7 @@ import universe from './universe.js';
 import npcManager from './npc-manager.js';
 import settingsManager from './settings-manager.js';
 import backgroundFx from './background-fx/background-fx.js';
+import gameManager from './game.js';
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -93,6 +94,7 @@ export default class Webaverse extends EventTarget {
         musicManager.waitForLoad(),
       ]);
     })();
+    gameManager.init();
     this.contentLoaded = false;
   }
   
