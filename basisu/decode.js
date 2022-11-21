38d3ce2379c73a56c5e800cde26abb69ea237b13
Jack Z <jakezira@outlook.com>
2022-11-21 09:33:37 -0800

    Cleanup

--- basisu/decode.js
+++ basisu/decode.js
@@ -5,12 +5,11 @@ export async function loadKtx2Texture(data) {
   const imageUrl = window.URL.createObjectURL(blob);
 
   const ktx2Loader = loaders.ktx2Loader;
-  const promise = new Promise((accept, reject) => {
+  const tex = await new Promise((accept, reject) => {
     ktx2Loader.load(imageUrl, function (texture) {
       accept(texture);
     });
   });
-  const tex = await promise;
 
   return tex;
 }
