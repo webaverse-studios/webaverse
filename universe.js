d66a065351fc7d91e23935404ae51384f2ade050
gonnavis <gonnavis@gmail.com>
2022-11-24 20:51:01 +0800

    Lint/Format.

--- universe.js
+++ universe.js
@@ -69,7 +69,7 @@ class Universe extends EventTarget {
         if (src === undefined) {
           const sceneNames = await sceneManager.getSceneNamesAsync();
           const sceneUrl = sceneManager.getSceneUrl(sceneNames[0]);
-          worldSpec = { src: sceneUrl };
+          worldSpec = {src: sceneUrl};
           promises.push(metaversefile.createAppAsync({
             start_url: sceneUrl,
           }));
