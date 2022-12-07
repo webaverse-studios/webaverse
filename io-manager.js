5a8eb3ea928045b7e8e819bc3da552dc8d335626
Patrick Bozic <patrickbozic@protonmail.com>
2022-12-07 17:47:41 +0800

    Fix bracket in io-manager.js

--- io-manager.js
+++ io-manager.js
@@ -431,6 +431,7 @@ class IoManager extends EventTarget {
             grabManager.menuRotate(1);
           } else {
             game.dropSelectedApp();
+          }
         }
         break;
       }
