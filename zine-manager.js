5cb5ee057e22d5808eb0d7bc7fb54c19f4d973a0
Avaer Kazmer <a@modules.io>
2022-12-13 15:28:03 -0500

    Dead code cleanup

--- zine-manager.js
+++ zine-manager.js
@@ -6,11 +6,6 @@ import {
 import {
   ZineRenderer,
 } from 'zine/zine-renderer.js';
-// export const panelSize = 1024;
-// export const floorNetWorldSize = 100;
-// export const floorNetWorldDepth = 1000;
-// export const floorNetResolution = 0.1;
-// export const floorNetPixelSize = floorNetWorldSize / floorNetResolution;
 import {
   panelSize,
   floorNetWorldSize,
