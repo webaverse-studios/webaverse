bb5c2bd1bf1c62717fe0b5a2f33e344c93e2b599
shawticus <shawmakesmagic@gmail.com>
2022-12-14 21:06:45 -0800

    If app has "invincible" component dont load the hit tracker when adding

--- world.js
+++ world.js
@@ -34,8 +34,12 @@ export class World {
       bindScene.add(app);
       let boundAppManager = this.appManager;
 
+      const isInvincible = app.getComponent('invincible');
+
+      console.log('is invincible', isInvincible)
+
       // regular glb models default to invincible for now
-      if (app.appType !== 'glb') {
+      if (!isInvincible) {
         const hitTracker = hpManager.makeHitTracker();
         hitTracker.bind(app);
         app.dispatchEvent({type: 'hittrackeradded'});
