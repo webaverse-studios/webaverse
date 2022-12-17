bcb8f8b528a9d4f87a9eb4a46b3e1297784d2346
Avaer Kazmer <a@modules.io>
2022-12-17 13:28:07 -0500

    Add gen-certs.sh

---
+++ scripts/gen-certs.sh
@@ -0,0 +1,1 @@
+sudo certbot certonly --manual --preferred-challenges dns -d local.webaverse.com,local-compiler.webaverse.com,local-renderer.webaverse.com,local.webaverse.live,local.webaverse.online,ai.webaverse.com
