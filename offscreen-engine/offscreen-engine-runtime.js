37857d9b2489e0c2a3827560a7259b5a10710daf
Avaer Kazmer <a@modules.io>
2022-12-17 21:21:45 -0500

    Offscreen engine runtime break-out

---
+++ offscreen-engine/offscreen-engine-runtime.js
@@ -0,0 +1,31 @@
+// import {
+//   zbencode,
+// } from 'zjs';
+import {bindCanvas} from '../renderer.js';
+import physx from '../physx.js';
+import physxWorkerManager from '../physx-worker-manager.js';
+import Avatar from '../avatars/avatars.js';
+
+import {
+  makeLocalWorker,
+} from './offscreen-engine.js';
+import offscreenEngineApi from './offscreen-engine-api.js';
+
+console.log('offscreen engine api run...');
+offscreenEngineApi(async (funcName, args, opts) => {
+  const worker = makeLocalWorker();
+
+  const _waitForLoad = async () => {
+    const canvas = document.getElementById('canvas');
+    bindCanvas(canvas);
+
+    await physx.waitForLoad();
+    await physxWorkerManager.waitForLoad();
+    await Avatar.waitForLoad();
+    await worker.waitForLoad();
+  };
+
+  await _waitForLoad();
+  const result = await worker.request(funcName, args);
+  return result;
+});
