8b20a5267ae5aa68021e8a7c1c87655497d7eaa5
Avaer Kazmer <a@modules.io>
2022-12-17 20:28:17 -0500

    Offscreen engine support remote rendering

--- offscreen-engine/offscreen-engine.js
+++ offscreen-engine/offscreen-engine.js
@@ -1,9 +1,15 @@
 import EngineWorker from './engine-worker.js?worker';
 import {getRandomString} from '../util.js';
+import {zbencode, zbdecode} from 'zjs';
 
 //
 
-const makeLocalWorker = () => {
+const rendererHost = `https://local-renderer.webaverse.com`;
+const localhost = `https://local.webaverse.com`;
+
+//
+
+export const makeLocalWorker = () => {
   const worker = new LocalEngineWorker();
 
   const messageChannel = new MessageChannel();
@@ -38,6 +44,63 @@ const makeLocalWorker = () => {
 
   return worker;
 };
+export const makeRemoteWorker = () => {
+  return new RemoteEngineWorker();
+};
+
+//
+
+class RemoteEngineWorker {
+  constructor() {
+    // nothing
+  }
+  async waitForLoad() {
+    // nothing
+  }
+  async request(funcName, args = [], {
+    signal = null,
+  } = {}) {
+    console.log('remote engine request', {
+      funcName,
+      args,
+    });
+
+    const u = new URL(`${rendererHost}/api`);
+    const start_url = `${localhost}/engine.html`;
+    u.searchParams.set('start_url', start_url);
+    u.searchParams.set('funcName', funcName);
+    // u.searchParams.set('args', JSON.stringify(args));
+    // cache ?? u.searchParams.set('cache', cache);
+    
+    const body = zbencode(args);
+
+    const res = await fetch(u, {
+      method: 'POST',
+      body,
+    });
+    // console.log('got res', res);
+    if (res.ok) {
+      const arrayBuffer = await res.arrayBuffer();
+      const uint8Array = new Uint8Array(arrayBuffer);
+      // console.log('got uint8Array', uint8Array);
+      try {
+        const result = zbdecode(uint8Array);
+        // console.log('got result', result);
+        return result;
+      } catch(err) {
+        // console.warn(err);
+        const textDecoder = new TextDecoder();
+        const text = textDecoder.decode(uint8Array);
+        console.warn('offscreen engine error', err, text);
+      }
+    } else {
+      throw new Error('failed to fetch: ' + res.status);
+    }
+  }
+  destroy() {
+    // nothing
+  }
+}
 
 //
 
