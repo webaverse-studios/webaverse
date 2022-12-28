05e7589f059c82505f6de9a1e761337e2d97b721
Avaer Kazmer <a@modules.io>
2022-12-28 17:19:27 -0500

    Add missing spawn-manager.js

---
+++ spawn-manager.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import {camera} from './renderer.js';
+import {playersManager} from './players-manager.js';
+import {partyManager} from './party-manager.js';
+
+//
+
+const localVector = new THREE.Vector3();
+const localVector2 = new THREE.Vector3();
+
+//
+
+class SpawnManager {
+  #spawnPosition = new THREE.Vector3();
+  #spawnQuaternion = new THREE.Quaternion();
+  setSpawnPoint(position, quaternion) {
+    this.#spawnPosition.copy(position);
+    this.#spawnQuaternion.copy(quaternion);
+  }
+  async spawn() {
+    const localPlayer = playersManager.getLocalPlayer();
+    // if the avatar was not set, we'll need to set the spawn again when it is
+    if (!localPlayer.avatar) {
+      await new Promise((accept, reject) => {
+        localPlayer.addEventListener('avatarchange', e => {
+          const {avatar} = e;
+          if (avatar) {
+            accept();
+          }
+        });
+      });
+    }
+    const {height} = localPlayer.avatar;
+    if (typeof height !== 'number') {
+      console.warn('no height', {localPlayer, height});
+      debugger;
+    }
+    const playerSpawnPosition = this.#spawnPosition.clone()
+      .add(
+        new THREE.Vector3(0, height, 0)
+      );
+    localPlayer.characterPhysics.setPosition(playerSpawnPosition);
+    camera.quaternion.copy(this.#spawnQuaternion);
+    camera.updateMatrixWorld();
+
+    // position all party members offset to spawnpoint
+    const partyMembers = partyManager.getPartyPlayers();
+    const diff = localVector.subVectors(this.#spawnPosition, localPlayer.position);
+    for (const player of partyMembers) {
+      const playerPosition = localVector2.addVectors(diff, player.position);
+      player.setSpawnPoint(playerPosition, quaternion);
+    }
+  }
+}
+const spawnManager = new SpawnManager();
+export default spawnManager;
