f4ad6833fe6f69667ead4934b5fb9ee4f8cef96e
Ron <rondoor124@gmail.com>
2023-02-01 02:41:00 +0200

    fix movement issue

--- packages/multiplayer/public/game.js
+++ packages/multiplayer/public/game.js
@@ -504,6 +504,7 @@ export const startGame = async ({
   _initRenderers();
 
   const _initLogic = () => {
+    debugger
     // world
     // const worldItemRenderers = [];
     // bind
@@ -604,8 +605,9 @@ export const startGame = async ({
     });
 
     // wait for the network to be ready befor binding controls
-    // realms.addEventListener('networkreconfigure', e => {
-    // }, {once: true});
+    realms.addEventListener('networkreconfigure', e => {
+      debugger
+    }, {once: true});
   };
   _initLogic();
 
@@ -739,7 +741,7 @@ export const startGame = async ({
         });
 
         // draw the world
-        const gameEl = document.getElementById(`game`);
+        const gameEl = document.getElementById(`engine-canvas`);
         const localPlayerEl = document.getElementById(`player-${realms.playerId}`);
         const worldAppsEl = document.getElementById('world-apps');
         const networkRealmsEl = document.getElementById('network-realms');
