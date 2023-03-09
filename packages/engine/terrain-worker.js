5909ee44d04e94806a1473cf0aff74085ae1e83d
tcm390 <tcm390@nyu.edu>
2023-03-09 10:25:53 -0500

    update worker

--- packages/engine/terrain-worker.js
+++ packages/engine/terrain-worker.js
@@ -54,6 +54,13 @@ onmessage = async function(event) {
   const iterationsOffsets = event.data.iterationsOffsets;
 
   const maxIterations = event.data.maxIterations;
+
+  const bounding = event.data.bounding;
+  const xMin = bounding.xMin;
+  const xMax = bounding.xMax;
+  const zMin = bounding.zMin;
+  const zMax = bounding.zMax;
+ 
   
   if (loaded) {
     scratchStack.f32[0] = size;
@@ -70,9 +77,14 @@ onmessage = async function(event) {
     scratchStack.f32[11] = elevationOffset;
     scratchStack.f32[12] = maxIterations;
 
+    scratchStack.f32[13] = xMin;
+    scratchStack.f32[14] = xMax;
+    scratchStack.f32[15] = zMin;
+    scratchStack.f32[16] = zMax;
+
     
     let iterationsOffsetsIndex = 0;
-    for (let i = 13; i < 13 + maxIterations * 2; i += 2) {
+    for (let i = 17; i < 17 + maxIterations * 2; i += 2) {
       scratchStack.f32[i] = iterationsOffsets[iterationsOffsetsIndex][0];
       scratchStack.f32[i + 1] = iterationsOffsets[iterationsOffsetsIndex][1];
       iterationsOffsetsIndex ++;
@@ -88,13 +100,13 @@ onmessage = async function(event) {
     const positionsSize = segments * segments * 3 + skirtCount * 3;
     const positionsArray = new Float32Array(positionsSize);
     const positionsStartIndex = 0; 
-    const positionsEndIndex = positionsSize; 
+    const positionsEndIndex = positionsStartIndex + positionsSize; 
     positionsArray.set(scratchStack.f32.subarray(positionsStartIndex, positionsEndIndex), 0);
     
     const normalsSize = segments * segments * 3 + skirtCount * 3;
     const normalsArray = new Float32Array(normalsSize);
     const normalsStartIndex = positionsEndIndex; 
-    const normalsEndIndex = positionsEndIndex + normalsSize; 
+    const normalsEndIndex = normalsStartIndex + normalsSize; 
     normalsArray.set(scratchStack.f32.subarray(normalsStartIndex, normalsEndIndex), 0);
 
 
@@ -102,21 +114,43 @@ onmessage = async function(event) {
     const indicesSize = indicesNumber * 6 + subdivisions * 4 * 6 * 4;
     const indicesArray = new Uint32Array(indicesSize);
     const indicesStartIndex = normalsEndIndex; 
-    const indicesEndIndex = normalsEndIndex + indicesSize; 
+    const indicesEndIndex = indicesStartIndex + indicesSize; 
     indicesArray.set(scratchStack.f32.subarray(indicesStartIndex, indicesEndIndex), 0);
     
 
     const textureSize = segments * segments * 4;
     const textureArray = new Float32Array(textureSize)
     const textureStartIndex = indicesEndIndex; 
-    const textureEndIndex = indicesEndIndex + textureSize; 
+    const textureEndIndex = textureStartIndex + textureSize; 
     textureArray.set(scratchStack.f32.subarray(textureStartIndex, textureEndIndex), 0);
 
     const uvSize = segments * segments * 2 + skirtCount * 2;
     const uvArray = new Float32Array(uvSize);
     const uvStartIndex = textureEndIndex; 
-    const uvEndIndex = textureEndIndex + uvSize; 
+    const uvEndIndex = uvStartIndex + uvSize; 
     uvArray.set(scratchStack.f32.subarray(uvStartIndex, uvEndIndex), 0);
+
+
+    const biomeWeightSize = segments * segments * 4;
+    const biomeWeightArray = new Float32Array(biomeWeightSize);
+    const biomeWeightStartIndex = uvEndIndex; 
+    const biomeWeightEndIndex = biomeWeightStartIndex + biomeWeightSize; 
+    biomeWeightArray.set(scratchStack.f32.subarray(biomeWeightStartIndex, biomeWeightEndIndex), 0);
+
+    const grassPositionsSize = scratchStack.f32[biomeWeightEndIndex];
+    const grassPositionsArray = new Float32Array(grassPositionsSize);
+    const grassPositionsStartIndex = biomeWeightEndIndex + 1;
+    const grassPositionsEndIndex = grassPositionsStartIndex + grassPositionsSize; 
+    grassPositionsArray.set(scratchStack.f32.subarray(grassPositionsStartIndex, grassPositionsEndIndex), 0);
+
+    const grassTerrainSlopesSize = scratchStack.f32[grassPositionsEndIndex];
+    const grassTerrainSlopesArray = new Float32Array(grassTerrainSlopesSize);
+    const grassTerrainSlopesStartIndex = grassPositionsEndIndex + 1;
+    const grassTerrainSlopesEndIndex = grassTerrainSlopesStartIndex + grassTerrainSlopesSize; 
+    grassTerrainSlopesArray.set(scratchStack.f32.subarray(grassTerrainSlopesStartIndex, grassTerrainSlopesEndIndex), 0);
+
+    // console.log(grassPositionsSize, grassPositionsArray, grassTerrainSlopesSize, grassTerrainSlopesArray);
+
 
     // Post
     postMessage({
@@ -126,6 +160,9 @@ onmessage = async function(event) {
       indices: indicesArray,
       texture: textureArray,
       uv: uvArray,
+      biomeWeight: biomeWeightArray,
+      grassPositions: grassPositionsArray,
+      grassTerrainSlopes: grassTerrainSlopesArray,
     })
   }
 
